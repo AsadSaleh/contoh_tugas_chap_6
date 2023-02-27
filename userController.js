44e206eb3258bd6c88458cfb5e27307af41cc322
As'ad Saleh Umar <azzzad.s@gmail.com>
2023-02-27 20:03:39 +0700

    Create boilerplate for userController

--- userController.js
+++ userController.js
@@ -1,8 +1,37 @@
-const getAllUsers = () => {};
-const getSingleUser = () => {};
-const createUser = () => {};
-const editUser = () => {};
-const deleteUser = () => {};
+const getAllUsers = (req, res) => {
+  // Baca list users dari database
+  const users = bacaDatabase();
+  res.json(users);
+};
+
+const getSingleUser = (req, res) => {
+  const id = req.params.id;
+
+  const user = bacaDatabase(id);
+  res.json(user);
+};
+
+const createUser = (req, res) => {
+  const newUser = req.body;
+
+  createUserKeDatabase(newUser);
+  res.json({ status: "create user berhasil" });
+};
+
+const editUser = (req, res) => {
+  const id = req.params.id;
+  const newUser = req.body;
+
+  updateUserKeDatabase(id, newUser);
+  res.json({ status: "edit user berhasil" });
+};
+
+const deleteUser = (req, res) => {
+  const id = req.params.id;
+
+  deleteuserKeDatabase(id);
+  res.json({ status: "user berhasil dihapus" });
+};
 
 module.exports = {
   getAllUsers,
